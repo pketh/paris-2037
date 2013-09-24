55388240da9956b68d64cd1a403fd21f841cea66
Pirijan Keth <pirijan@gmail.com>
2013-09-24 12:03:48 -0400

    Initial build partway complete

---
+++ README.md
@@ -0,0 +1,42 @@
+# Paris 2013
+
+= replace following:
+Kaomojis are like emojis but in the form of text strings like: 
+this happy guy ＼（＠￣∇￣＠）／ <br>
+or 
+this dude flipping over a table (╯°□°）╯︵ ┻━┻
+
+Kaomoji Cat is a webapp that helps you find the perfect kaomoji to express how you're feeling. 
+
+While there are way more kaomojis than emojis, kaomojis can be more flexible and expressive. I played off that quirky expressiveness with the character of the Kaomoji cat, fish and red string. The cat and his app hopefully comes across as cute, precocious and maybe a little bit creepy.
+
+[Try it](http://kaomoji.comoj.com/ "See Kaomoji Cat")<br>
+[More details](http://pketh.org/Kaomoji-Cat)
+
+![screenshot](http://payload100.cargocollective.com/1/0/1410/4319041/photo-4.png)
+
+
+-------------------------
+
+experiment in interactive poster design
+
+wanted to do everything from scratch in the lightest weight way . so no external libs , just jquery. High in DRY.
+
+
+more comfortable with the illustrator pen tool than a paint brush
+
+my roots are in vector based illustration
+
+paris 2037 is a web illustration experiment
+cinematic rather than interactive
+place to play with negative space illustration and css/jquery animation
+dipping my toes into making an illustration come to life in a web native way
+
+element of abstract ness to it
+
+theme pf paris. started with a worlds fair and that glamour
+name = started as a poster for a world’s fair based the one in paris in 1937
+
+: at end: gotta make a favicon
+
+: ADD media queries for comets at small view sizes
---
+++ public/js/script.js
@@ -0,0 +1,53 @@
+$( document ).ready(function() {
+
+  // vertically center title
+  var height = $(window).height();
+  var width = $(window).width();
+  
+  var headerHeightSplit = ((height - 75) / 2);
+  // apply to css "header"
+  $(".header-img").css({
+    "margin-top" : headerHeightSplit ,
+    "margin-bottom" : headerHeightSplit
+  });
+  
+  var scrollWidthSplit = ((width - 43) / 2);
+  $('.scroll-down').css({
+    "left": scrollWidthSplit,
+    "top": height - 50
+  });
+
+  // fade out arrow once scrolling happens  
+  $(document).scroll(function() {
+    $('.scroll-down').removeClass('down-animation');  
+    $('.scroll-down').addClass('hidden');
+  });
+
+  // horizontally center topstars
+  var topStarsWidthSplit = ((width - 796) / 2)
+  $('.topstars').css({
+    "left": topStarsWidthSplit
+  });
+
+
+  // parallax circles
+  $(window).on("scroll",function(){
+    $('.circle1').css({"top" : ( .5 * $(window).scrollTop() )      })
+    $('.circle2').css({"top" : ( .2 * $(window).scrollTop() ) +200 })
+    $('.circle3').css({"top" : ( .4 * $(window).scrollTop() ) +20  })
+    $('.circle4').css({"top" : ( .3 * $(window).scrollTop() ) +150 })
+    $('.topstar2').css({"top": ( .1 * $(window).scrollTop() ) -20  })
+
+    // If I want the parallax balls to also fade out as you scroll ...
+    // $('.circle1').css({"top" : ( .5 * $(window).scrollTop() ) ,        "opacity" : ($(window).height() / $(document).scrollTop() ) -.5 })
+    // $('.circle2').css({"top" : ( .2 * $(window).scrollTop() ) +200,    "opacity" : ($(window).height() / $(document).scrollTop() ) -.5 })
+    // $('.circle3').css({"top" : ( .4 * $(window).scrollTop() ) +20 ,    "opacity" : ($(window).height() / $(document).scrollTop() ) -.5 })
+    // $('.circle4').css({"top" : ( .3 * $(window).scrollTop() ) +150,    "opacity" : ($(window).height() / $(document).scrollTop() ) -.5 })
+  });
+
+
+
+}); // end of document ready
+
+
+
